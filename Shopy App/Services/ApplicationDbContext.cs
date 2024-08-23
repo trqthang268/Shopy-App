bb8ece13c6ee3f5a1e708cc35e27c184545c2967
Your Name <truongthang268@gmail.com>
2024-08-23 17:17:02 +0700

    Add project files.

---
+++ Shopy App/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopy_App.Models;
+
+namespace Shopy_App.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
